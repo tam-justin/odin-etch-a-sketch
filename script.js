28fb83595c2fe92914ce9dc8cf7e81f12784c3a8
tam-justin <144107518+tam-justin@users.noreply.github.com>
2025-06-09 19:22:04 -0700

    menu bar and board

---
+++ script.js
@@ -0,0 +1,88 @@
+const sizeSlider = document.getElementById("size-slider");
+const sizeText = document.getElementById("board-size");
+const board = document.getElementById("board");
+
+let mode = "singleColor";
+
+let border = false;
+
+const singleButton = document.getElementById("single");
+const colorPicker = document.getElementById("color-wheel");
+const rainbowButton = document.getElementById("rainbow");
+const eraserButton = document.getElementById("eraser");
+const clearButton = document.getElementById("clear");
+const borderButton = document.getElementById("border");
+
+function activate(newMode){
+    if(mode === "singleColor"){
+        singleButton.classList.remove("active");
+    }else if(mode === "rainbow"){
+        rainbowButton.classList.remove("active");
+    }else if(mode === "eraser"){
+        eraserButton.classList.remove("active");
+    }
+
+    if(newMode === "singleColor"){
+        singleButton.classList.add("active");
+        mode = "singleColor";
+    }else if(newMode === "rainbow"){
+        rainbowButton.classList.add("active");
+        mode = "rainbow";
+    }else if(newMode === "eraser"){
+        eraserButton.classList.add("active");
+        mode = "eraser";
+    }
+}
+
+singleButton.addEventListener("click", () => {
+    activate("singleColor");
+})
+
+rainbowButton.addEventListener("click", () => {
+    activate("rainbow");
+})
+
+eraserButton.addEventListener("click", () => {
+    activate("eraser");
+})
+
+borderButton.addEventListener("click", () => {
+    if(border == true){
+        const arr = document.querySelectorAll(".units");
+        for(let i = 0; i < arr.length; i++){
+            arr[i].style.border = "none";
+        }
+        borderButton.textContent = "show border";
+        border = false;
+    }else{
+        const arr = document.querySelectorAll(".units");
+        for(let i = 0; i < arr.length; i++){
+            arr[i].style.border = "1px solid black";
+        }
+        borderButton.textContent = "hide border";
+        border = true;
+    }
+})
+
+function createBoard(size){
+    for(let i = 0; i < size*size; i++){
+        const unit = document.createElement("div");
+        unit.classList.add("units");
+        board.appendChild(unit);
+        console.log(i);
+    }
+}
+
+function updateSize(size){
+    board.innerHTML = "";
+    document.documentElement.style.setProperty('--size', size);
+    createBoard(size);
+}
+
+sizeSlider.addEventListener("input", () => {
+    sizeText.textContent = `${sizeSlider.value} x ${sizeSlider.value}`;
+    updateSize(sizeSlider.value);
+});
+
+
+document.onload(createBoard(16));
